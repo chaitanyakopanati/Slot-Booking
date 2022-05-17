f5d51d5d5a90ec68b8ad4e884a178ebf2d3e5e20
sumit <sumit.disolutions@gmail.com>
2022-05-17 11:20:43 +0530

    project setup

---
+++ src/app/routing/PrivateRoutes.tsx
@@ -0,0 +1,34 @@
+import {lazy, FC, Suspense} from 'react'
+import {Route, Routes, Navigate} from 'react-router-dom'
+import {MasterLayout} from '../../_metronic/layout/MasterLayout'
+import TopBarProgress from 'react-topbar-progress-indicator'
+import {DashboardWrapper} from '../pages/dashboard/DashboardWrapper'
+import {getCSSVariableValue} from '../../_metronic/assets/ts/_utils'
+
+const PrivateRoutes = () => {
+  return (
+    <Routes>
+      <Route element={<MasterLayout />}>
+        {/* Redirect to Dashboard after success login/registartion */}
+        <Route path='auth/*' element={<Navigate to='/dashboard' />} />
+        {/* Pages */}
+        <Route path='dashboard' element={<DashboardWrapper />} />
+        <Route path='*' element={<Navigate to='/error/404' />} />
+      </Route>
+    </Routes>
+  )
+}
+
+const SuspensedView: FC = ({children}) => {
+  const baseColor = getCSSVariableValue('--bs-primary')
+  TopBarProgress.config({
+    barColors: {
+      '0': baseColor,
+    },
+    barThickness: 1,
+    shadowBlur: 5,
+  })
+  return <Suspense fallback={<TopBarProgress />}>{children}</Suspense>
+}
+
+export {PrivateRoutes}
